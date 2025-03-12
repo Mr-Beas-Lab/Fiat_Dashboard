886989b70902a4e8e8cc46a3a19713d9a21cf1ac
million-art <millionmulugeta09@gmail.com>
2025-03-12 22:18:58 +0300

    fix build

--- src/components/ambassador/AmbassadorList.tsx
+++ src/components/ambassador/AmbassadorList.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import type React from "react"
 import type { Ambassador } from "../../types"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table"
